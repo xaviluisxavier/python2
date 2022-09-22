0199d7de3a3d2a89927467f1d8d994fabec7c651
Luis <luispacheco_08@hotmail.com>
2022-09-22 12:07:14 +0000

    implementa várias itemeticas

--- programa.py
+++ programa.py
@@ -5,12 +5,32 @@ Este programa implementa funções ortméticas
 """
 
 
-def somar(valor1, valor2):
-    total = valor1 + valor2
+def aritmetica(valor1, valor2, op='+'):
+    """
+    Esta função inplementa as operações de somar, subtrair, multiplicar e dividir
+    :param valor1: Primeiro fator de operação
+    :param valor2: Segundo fator de operação
+    :param op: Operação; valores validos são: + - : *
+    :return: Resultado da operação
+    """
+    total = "Código de operação inválido"
+    if op == '+':
+     total = valor1 + valor2
+
+    if op == '-':
+     total = valor1 - valor2
+
+    if op == '/':
+     total = valor1 / valor2
+
+    if op == '*':
+     total = valor1 * valor2
+
     return total
+
 
 
 if __name__ == '__main__':
     fator1 = 10
     fator2 = 20
-    print(f' {fator1} mais {fator2} = {somar(fator1, fator2)}')
+    print(f' {fator1} mais {fator2} = {aritmetica(fator1, fator2, "*")}')
