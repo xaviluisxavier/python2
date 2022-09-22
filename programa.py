88ad08ca0cbe5402c07b230864fef41a500385bf
Luis <luispacheco_08@hotmail.com>
2022-09-22 11:44:42 +0000

    implementa somar

---
+++ programa.py
@@ -0,0 +1,16 @@
+# isto é um comentário
+
+"""
+Este programa implementa funções ortméticas
+"""
+
+
+def somar(valor1, valor2):
+    total = valor1 + valor2
+    return total
+
+
+if __name__ == '__main__':
+    fator1 = 10
+    fator2 = 20
+    print(f' {fator1} mais {fator2} = {somar(fator1, fator2)}')
