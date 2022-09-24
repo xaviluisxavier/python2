0ddb8eca557e72f963c3674b9851b6b7dffee3c8
XAVI <30757322+xaviluisxavier@users.noreply.github.com>
2022-09-24 00:27:40 +0000

    Update problema2.py

--- problema2.py
+++ problema2.py
@@ -6,20 +6,21 @@ Mostra aquela quantidade de numeros primos a partir do numero inicial.
 
 
 if __name__ == '__main__':
- numero_prim = 0
- continuar = 's'
+numero_prim = 0
+continuar = 's'
 while continuar == 's':
     inicio = int(input('Insira o primeiro numero: '))
-    fim = int(input('Insira o segundo numero: '))
+    qnt_primos = int(input('Insira quantidade de primos: '))
 
-    for n in range(inicio, fim + 1):
-        if n > 1:
-            for j in range(2, n):
-                if n % j == 0:
-                    break
-            else:
-                print(n)
-                numero_prim += numero_prim 
-    print(numero_prim)
-    continuar = input('repetir [s | n]?')
+    for n in range(inicio, 100):
+        if numero_prim < qnt_primos:
+            if n > 1:
+                for j in range(qnt_primos, n):
+                    if n % j == 0:
+                        break
+                else:
+                    print(n)
+                    numero_prim += 1
+        else:
+            break
 print(f'Adeus')
