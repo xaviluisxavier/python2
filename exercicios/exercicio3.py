b8b39eaefb5d5da5c0d9f0bd38ff27c7501ff0ef
XAVI <30757322+xaviluisxavier@users.noreply.github.com>
2022-09-26 19:18:26 +0000

    Update exercicio3.py

--- exercicios/exercicio3.py
+++ exercicios/exercicio3.py
@@ -20,11 +20,49 @@ def get_random(ini, fim):
 
 
 def par(num):
-    if num % 2 == 0:
-        return num
+  return num % 2 == 0
+
+
+def impar(num):
+  return num % 2 != 0
 
 
 if __name__ == '__main__':
+  num_ini = int(input(f'Qual é o número inicial? '))
+  num_end = int(input(f'Qual é o número final? '))
+  opcao = int(input(f'Qual opção deseja [par - 1, impar - 2, primo - 3]? '))
+
+  num_random = 0
+  list_random = []
+  list_random_par = []
+  list_random_impar = []
+  list_random_primo = []
+
+  for x in range(num_ini, num_end + 1):
+    num_random = get_random(num_ini, num_end)
+
+    list_random.insert(x, num_random)
+
+    if opcao == 1:
+      if par(num_random):
+        list_random_par.insert(x, num_random)
+
+    elif opcao == 2:
+      if impar(num_random):
+        list_random_impar.insert(x, num_random)
+
+    elif opcao == 3:
+      if impar(num_random):
+        list_random_primo.insert(x, num_random)
+
+  print(f'Lista {list_random}')
+
+  if opcao == 1:
+    print(f'Lista Par {list_random_par}')
+  elif opcao == 2:
+    print(f'Lista Impar {list_random_impar}')
+  elif opcao == 3:
+    print(f'Lista Primos {list_random_primo}')
     num_ini = int(input('Qual é o número inicial? '))
     num_end = int(input('Qual é o número final? '))
     opcao = int(input('Qual é a opção (1 - pares, 2 = impares, 3 - primos)'))
