2044b9eaf9cce5277a8c71934d635c8259fb268e
XAVI <30757322+xaviluisxavier@users.noreply.github.com>
2022-09-27 21:10:12 +0000

    Update exercicio4.py

--- exercicios/exercicio4.py
+++ exercicios/exercicio4.py
@@ -1,31 +1,35 @@
 """
-Declare uma lista para guardar as vendas do grupo central
-Declara uma lista para guardar os nomes das cinco ilhas do grupo central
-Peça dados ao utilizador e guarde-os na lista
-Após o utilizador ter inserido os 5 valores apresente:
-- Total das vendas
+Declare uma lista para guardar as vendas do grupo central feito
+Declara uma lista para guardar os nomes das cinco ilhas do grupo central feito
+Peça dados ao utilizador e guarde-os na lista feito
+Após o utilizador ter inserido os 5 valores apresente: feito
+- Total das vendas feito
 - O menor valor inserido assim como as respetivas ilhas
 - O maior valor inserido assim como as respetivas ilhas
-- A média das vendas
+- A média das vendas feito
 """
-
-ilhas = ['Terceira', 'Graciosa', 'Pico', 'Faial', 'São Jorge']
-
 if __name__ == '__main__':
-    vendas = []
-    for ilha in ilhas:
-        vendas.append((int(input(f'Insira as vendas para a ilhda da {ilha} '))))
-    print(f'vendas={vendas}')
+    #print(f'Incira 5 valores')
 
-    menor = vendas[0]
-    maior = vendas[0]
+    ListadeVendas = []
+    Tamanho = ListadeVendas.__len__()
+    ListadeNomes = ['Terceira', 'Pico', 'Faial', 'São Jorge', 'Graciosa']
 
-    print(f'O maior é {max(vendas)} e o menor é {min(vendas)}.')
+    for ilha in ListadeNomes:
+        ListadeVendas.append((int(input(f'Insira as vendas para {ilha} '))))
+    print(f'vendas={ListadeVendas}')
 
+    soma = sum(ListadeVendas)
+    minimo = min(ListadeVendas)
+    maximo = max(ListadeVendas)
     total = 0
-    for v in vendas:
+    for v in ListadeVendas:
         total += v
 
-    media = total / len(vendas)
+    media = total / len(ListadeVendas)
 
-    print(f'O total é {sum(vendas)} e a média é {media}.')
+    print(f'Total da soma é {soma}')
+    print(f'A venda mais alta é {maximo} e vem da ilha {ListadeNomes}')
+    print(f'A venda mais baixa foi {minimo} e vem da ilha {ListadeNomes}')
+    print(f'A media é de {media}')
+    print(f'A lista de vendas é {ListadeVendas}')
