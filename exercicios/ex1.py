80196e54d782cdc949deb38a3203296197b6b3b5
XAVI <30757322+xaviluisxavier@users.noreply.github.com>
2022-09-28 09:02:43 +0000

    Update ex1.py

--- exercicios/ex1.py
+++ exercicios/ex1.py
@@ -1,14 +1,14 @@
 def maior(num1, num2):
     if num1 > num2:
         print(f'numero {num1} é maior')
-     elif:
+    else:
         print(f'numero {num2} é maior')
 
 
 def menor(num1, num2):
     if num1 < num2:
         print(f'numero {num1} é menor')
-     elif:
+    else:
         print(f'numero {num2} é menor')
 
 
