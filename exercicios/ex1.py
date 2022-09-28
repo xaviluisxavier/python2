94e313cf1c2e6c7b1286914828d2c14160eb1961
Luis <luispacheco_08@hotmail.com>
2022-09-28 08:52:09 +0000

    .

---
+++ exercicios/ex1.py
@@ -0,0 +1,18 @@
+def maior(num1, num2):
+
+
+
+if __name__ == '__main__':
+    continuar = 's'
+    while continuar == 's':
+        primeiro_n = int(input('Insira o primeiro numero '))
+        segundo_n = int(input('Insira o segundo numero '))
+        if primeiro_n > segundo_n:
+            print(f'O numero {primeiro_n} é maior')
+        if primeiro_n < segundo_n:
+            print(f'O numero {primeiro_n} é menor')
+        if primeiro_n == segundo_n:
+            print(f'o numero {primeiro_n} é igual ao numero {segundo_n}')
+
+        continuar = input('Repetir [s | n]? ')
+print(f'Adeus!')
