6abac3ffb6620e67a468ed133511457cdaf0b74f
XAVI <30757322+xaviluisxavier@users.noreply.github.com>
2022-09-28 09:08:11 +0000

    Update ex1.py

--- exercicios/ex1.py
+++ exercicios/ex1.py
@@ -1,20 +1,3 @@
-def maior(num1, num2):
-    if num1 > num2:
-        print(f'numero {num1} é maior')
-    else:
-        print(f'numero {num2} é maior')
-
-
-def menor(num1, num2):
-    if num1 < num2:
-        print(f'numero {num1} é menor')
-    else:
-        print(f'numero {num2} é menor')
-
-
-def igual(num1, num2):
-    if num1 == num2:
-        print(f'numero {num1} é igual ao {num2}')
 
 
 if __name__ == '__main__':
@@ -23,11 +6,11 @@ if __name__ == '__main__':
         primeiro_n = int(input('Insira o primeiro numero '))
         segundo_n = int(input('Insira o segundo numero '))
         if primeiro_n > segundo_n:
-            print(maior(primeiro_n,segundo_n))
+            print(f'O numero {primeiro_n} é maior')
         if primeiro_n < segundo_n:
-            print(menor(primeiro_n,segundo_n))
-        if primeiro_n == segundo_n:
-            print(igual(primeiro_n,segundo_n))
+            print(f'O numero {primeiro_n} é menor')
+        else:
+            print(f'o numero {primeiro_n} é igual ao numero {segundo_n}')
 
         continuar = input('Repetir [s | n]? ')
 print(f'Adeus!')
